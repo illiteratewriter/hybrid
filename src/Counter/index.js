5b50cb1242dadbf371aa6af75eb61545227ddd36
Manu Koshy Abraham <manukoshyabraham@Manus-MacBook-Pro.local>
2019-04-16 16:14:04 +0530

    feat(Structure)

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,32 +1,11 @@
 import React from "react"
 
-import Counter from './Counter'
+import CounterContainer from "./Containers/CounterContainer"
 
-class CounterWrapper extends React.Component {
-  state = {
-    counter : 0
-  }
-
-  increaseClick = () => {
-    this.setState({
-      counter: this.state.counter+1
-    })
-  }
-
-  decreaseClick = () => {
-    this.setState({
-      counter: this.state.counter-1
-    })
-  }
-
-  render(){
-    return (
-      <Counter
-        count={this.state.counter}
-        increase={this.increaseClick}
-        decrease={this.decreaseClick} />
-    )
-  }
+const Counter = props => {
+  return (
+    <CounterContainer {...props} />
+  )
 }
 
-export default CounterWrapper
+export default Counter
